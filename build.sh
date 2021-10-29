17d5ec5537987805f0dc07fe28e41f315dd4b122
AriadneAu <jah7274@psu.edu>
2021-10-29 15:10:12 -0400

    You didn't see me leave that example path in.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-[ ! -d "/path/to/dir" ] && echo "MISSING PAYLOAD DIRECTORY: Run ./mkpayload.sh" && exit 1
+[ ! -d "payload" ] && echo "MISSING PAYLOAD DIRECTORY: Run ./mkpayload.sh" && exit 1
 sudo docker build -t ros2_zed2:latest .
