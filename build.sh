eef52880a34e2147e2d8db23da4694f4737d7f02
AriadneAu <jah7274@psu.edu>
2021-10-29 15:16:01 -0400

    Fix directory name.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-[ ! -d "sysinfo" ] && echo "You are missing the sysinfo directory. Run ./mksysinfo.sh" && exit 1
+[ ! -d "sysinfo" ] && echo "You are missing the system-generated file directory. Run ./mksysinfo.sh" && exit 1
 sudo docker build -t ros2_zed2:latest .
