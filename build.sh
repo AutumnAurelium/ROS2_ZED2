f1ff1ba7354411214b63090c7ac06a576ca11315
AriadneAu <jah7274@psu.edu>
2021-10-29 15:16:38 -0400

    Fix typo

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-[ ! -d "sysinfo" ] && echo "You are missing the system-generated file directory. Run ./mksysinfo.sh" && exit 1
+[ ! -d "generated" ] && echo "You are missing the system-generated file directory. Run ./mksysinfo.sh" && exit 1
 sudo docker build -t ros2_zed2:latest .
