569edc9d68ce033781e8fd1e8052dbb0fd1681eb
AriadneAu <jah7274@psu.edu>
2021-11-01 19:39:41 -0400

    move variable

--- scripts/installColconPkgs.sh
+++ scripts/installColconPkgs.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 export ROS_DISTRO=foxy
-export CMAKE_SYSTEM_VERSION=focal # it's not but we have emulated the packages
 
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src/ #use your current ros2 workspace folder
 git clone https://github.com/stereolabs/zed-ros2-wrapper.git
 cd ..
 rosdep install --from-paths src --ignore-src -r -y
-colcon build --symlink-install --cmake-args=-DCMAKE_BUILD_TYPE=Release
+colcon build --symlink-install --cmake-args=-DCMAKE_BUILD_TYPE=Release -DCMAKE_OS_VERSION=focal
 echo source $(pwd)/install/local_setup.bash >> ~/.bashrc
 source ~/.bashrc
